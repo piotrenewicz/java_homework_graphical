3139be4574da5a04832f9cb9411fa8fe144c98e3
Piotrenewicz <piotrekm9@onet.eu>
2021-01-18 15:02:49 +0100

    cegły

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+//import java.awt.List;
 import java.awt.event.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.*;
+import java.util.List;
+
 class Kulka extends Ellipse2D.Float
 {
     Plansza p;
@@ -53,12 +57,28 @@ class SilnikKulki extends Thread
         catch(InterruptedException e){}
     }
 }
+class Cegielka extends Rectangle2D.Float
+{
+    Boolean active;
+
+    Cegielka(int x, int y, int w, int h){
+        this.x=x;
+        this.y=y;
+        this.active = true;
+        this.width=w;
+        this.height=h;
+    }
+}
+
 class Belka extends Rectangle2D.Float
 {
-    Belka(int x)
+    Plansza p;
+
+    Belka(Plansza p, int x)
     {
+        this.p = p;
         this.x=x;
-        this.y=170;
+        this.y=0;
         this.width=60;
         this.height=10;
     }
@@ -66,6 +86,7 @@ class Belka extends Rectangle2D.Float
     void setX(int x)
     {
         this.x=x;
+        this.y = p.getHeight()-20;
     }
 }
 class Plansza extends JPanel implements MouseMotionListener
@@ -73,15 +94,38 @@ class Plansza extends JPanel implements MouseMotionListener
     Belka b;
     Kulka a;
     SilnikKulki s;
+    List<Cegielka> cegly_na_planszy = new ArrayList<Cegielka>();
 
     Plansza()
     {
         super();
         addMouseMotionListener(this);
-
-        b=new Belka(100);
+        b=new Belka(this, 100);
         a=new Kulka(this,100,100,1,1);
         s=new SilnikKulki(a);
+    }
+
+    void post_init(){
+        populate_cegly();
+    }
+
+    void populate_cegly(){
+        int hpad = 10;
+        int vpad = 10;
+        int brick_w = 50;
+        int brick_h = 10;
+        int populating_x = 0;
+        int populating_y = 0;
+        for(int _y = 0; _y < 5; _y++){
+            populating_y += vpad;
+            for(int _x =0; _x< 5; _x++){
+                populating_x += hpad;
+                this.cegly_na_planszy.add(new Cegielka(populating_x, populating_y, brick_w, brick_h));
+                populating_x += brick_w;
+            }
+            populating_x = 0;
+            populating_y += brick_h;
+        }
     }
 
     public void paintComponent(Graphics g)
@@ -91,6 +135,9 @@ class Plansza extends JPanel implements MouseMotionListener
 
         g2d.fill(a);
         g2d.fill(b);
+        for(Cegielka to_draw : cegly_na_planszy){
+            if(to_draw.active) g2d.fill(to_draw);
+        }
     }
 
     public void mouseMoved(MouseEvent e)
